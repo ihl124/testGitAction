93c517410e27bd798ade823e009561f16ab2800d
ihl124 <ptq124@ihl.co.kr>
2025-04-18 14:32:27 +0900

    feat: 수정

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03821b46fbd9a5f15832e7620eae7185b2bce919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf45da7f51e41842345b6760cc6c8bef1be283c")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
